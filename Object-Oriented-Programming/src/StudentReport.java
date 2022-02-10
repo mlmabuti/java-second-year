def735954050ecb95712b117e6c471799bb281d8
wafforyu <wafforyu@gmail.com>
2022-02-11 03:14:30 +0800

    remove outdated comment

--- Object-Oriented-Programming/src/StudentReport.java
+++ Object-Oriented-Programming/src/StudentReport.java
@@ -1,9 +1,4 @@
 /*
-use constructors, add new csv data to test, refactor/shorten/simplify code, do something about null pointers
-remove utils
-object within an object might mean you should create address, degree, parent objects inside the student
-use arraylist and pairs
-
 NOTE: add public access modifiers if ever this project needs to be used somewhere else.
       Address, Parent, and Student constructors have been created to make cleaner looking code.
       did not deal with the possibility of readers to return NullPointerException
