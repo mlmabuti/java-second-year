f50f97b3c6d8ac3494d74d6405fd79598d8ff662
wafforyu <wafforyu@gmail.com>
2022-02-11 01:40:53 +0800

    minor changes

--- Object-Oriented-Programming/src/Address.java
+++ Object-Oriented-Programming/src/Address.java
@@ -1,16 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 class Address{
-    String location, addressType, address;
-    static ArrayList<Address> addresses;
+    ArrayList<Address> addresses = new ArrayList<>();
+    String location, addressType;
 
-    //Address(String location, String addressType){
-    //    this.location = location;
-    //    this.addressType = addressType;
-    //    this.address = "C".equals(addressType) ? "City" : "Provincial";
-    //}
+    Address() {}
 
-    void processAddress(){
+    Address(String location, String addressType){
+        this.location = location;
+        this.addressType = addressType;
+    }
 
+    ArrayList<Address> getAddresses(){ return this.addresses; }
+
+    void processAddress() {
+        final String pathAddressInfo = "studentData/AddressInfo.csv";
+        String rowAddress;
+        BufferedReader br = Utils.createBuffer(pathAddressInfo);
+
+        try {
+            while ((rowAddress= br.readLine()) != null) { // loop until current line in csv is not null
+                String[] rowAddressSpecific = rowAddress.split(","); // split each like into an array per ","
+
+                // address instance
+                Address a = new Address(
+                        rowAddressSpecific[2],
+                        rowAddressSpecific[1]);
+
+                // add address instance to list of addresses
+                addresses.add(a);
+
+            }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 }
