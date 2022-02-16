56283ae05647cffb334abad0af7f1d6d192b88f1
axojolotl <wafforyu@gmail.com>
2022-02-17 01:47:01 +0800

    add euclid's algo

---
+++ Design-And-Analysis-Of-Algorithms/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(gcd(60,24));
+        System.out.println(recursiveGcd(60, 24));
+    }
+
+    static int gcd(int m, int n){
+        while (n != 0){
+            int r = m % n;
+            m = n;
+            n = r;
+        }
+        return m;
+    }
+
+    static int recursiveGcd(int m, int n){
+        if (n == 0){ return m; }
+        return recursiveGcd(n, m % n);
+    }
+}
