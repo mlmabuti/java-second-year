00aa2a5f86e17f772be4b604f17bcc32913960dc
axojolotl <wafforyu@gmail.com>
2022-02-23 22:50:18 +0800

    write first proto

--- Design-And-Analysis-Of-Algorithm/assignment-3/src/Main.java
+++ Design-And-Analysis-Of-Algorithm/assignment-3/src/Main.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        int n = 20;
+        ArrayList <Integer> arr = new ArrayList<>();
+        for (int p = 2 ; p <= n; p ++ ){
+            arr.add(p);
+            if (arr.get(p) != 0){
+                int j = p;
+                while (j <= n){
+                    arr.set(j, 0);
+                    j = j + p;
+                }
+            }
+        }
+
+        System.out.println(arr.toString());
+    }
 }
